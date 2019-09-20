b65f137b8ab95f959213e1689d38b076fc034587
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2019-09-20 23:22:10 +0200

    Further install speedup

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-./gradlew install -x test -x checkThreshold -x apidoc -x htmlQualityReport
+./gradlew install -x test -x checkThreshold -x apidoc -x htmlQualityReport -x findbugsMain -x findbugsTest -x checkStyleMain -x checkStyleTest
