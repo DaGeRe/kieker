da665d3b8d63c1bde07de2a62f488a20953d3108
David Georg Reichelt <davidgeorg_reichelt@dagere.de>
2020-11-30 11:14:54 +0100

    Try to reduce captured data

--- kieker-monitoring/src/kieker/monitoring/probe/aspectj/operationExecution/AbstractOperationExecutionAspect.java
+++ kieker-monitoring/src/kieker/monitoring/probe/aspectj/operationExecution/AbstractOperationExecutionAspect.java
@@ -38,72 +38,72 @@ import kieker.monitoring.timer.ITimeSource;
  */
 @Aspect
 public abstract class AbstractOperationExecutionAspect extends AbstractAspectJProbe {
-	private static final Logger LOGGER = LoggerFactory.getLogger(AbstractOperationExecutionAspect.class);
+   private static final Logger LOGGER = LoggerFactory.getLogger(AbstractOperationExecutionAspect.class);
 
-	private static final IMonitoringController CTRLINST = MonitoringController.getInstance();
-	private static final ITimeSource TIME = CTRLINST.getTimeSource();
-	private static final String VMNAME = CTRLINST.getHostname();
-	private static final ControlFlowRegistry CFREGISTRY = ControlFlowRegistry.INSTANCE;
-	private static final SessionRegistry SESSIONREGISTRY = SessionRegistry.INSTANCE;
+   private static final IMonitoringController CTRLINST = MonitoringController.getInstance();
+   private static final ITimeSource TIME = CTRLINST.getTimeSource();
+   // private static final String VMNAME = CTRLINST.getHostname();
+   private static final ControlFlowRegistry CFREGISTRY = ControlFlowRegistry.INSTANCE;
+   // private static final SessionRegistry SESSIONREGISTRY = SessionRegistry.INSTANCE;
 
-	/**
-	 * The pointcut for the monitored operations. Inheriting classes should extend the pointcut in order to find the correct executions of the methods (e.g. all
-	 * methods or only methods with specific annotations).
-	 */
-	@Pointcut
-	public abstract void monitoredOperation();
+   /**
+    * The pointcut for the monitored operations. Inheriting classes should extend the pointcut in order to find the correct executions of the methods (e.g. all methods or only
+    * methods with specific annotations).
+    */
+   @Pointcut
+   public abstract void monitoredOperation();
 
-	@Around("monitoredOperation() && notWithinKieker()")
-	public Object operation(final ProceedingJoinPoint thisJoinPoint) throws Throwable { // NOCS (Throwable)
-		if (!CTRLINST.isMonitoringEnabled()) {
-			return thisJoinPoint.proceed();
-		}
-		final String signature = this.signatureToLongString(thisJoinPoint.getSignature());
-		if (!CTRLINST.isProbeActivated(signature)) {
-			return thisJoinPoint.proceed();
-		}
-		// collect data
-		final boolean entrypoint;
-		final String hostname = VMNAME;
-		final String sessionId = SESSIONREGISTRY.recallThreadLocalSessionId();
-		final int eoi; // this is executionOrderIndex-th execution in this trace
-		final int ess; // this is the height in the dynamic call tree of this execution
-		long traceId = CFREGISTRY.recallThreadLocalTraceId(); // traceId, -1 if entry point
-		if (traceId == -1) {
-			entrypoint = true;
-			traceId = CFREGISTRY.getAndStoreUniqueThreadLocalTraceId();
-			CFREGISTRY.storeThreadLocalEOI(0);
-			CFREGISTRY.storeThreadLocalESS(1); // next operation is ess + 1
-			eoi = 0;
-			ess = 0;
-		} else {
-			entrypoint = false;
-			eoi = CFREGISTRY.incrementAndRecallThreadLocalEOI(); // ess > 1
-			ess = CFREGISTRY.recallAndIncrementThreadLocalESS(); // ess >= 0
-			if ((eoi == -1) || (ess == -1)) {
-				LOGGER.error("eoi and/or ess have invalid values: eoi == {} ess == {}", eoi, ess);
-				CTRLINST.terminateMonitoring();
-			}
-		}
-		// measure before
-		final long tin = TIME.getTime();
-		// execution of the called method
-		final Object retval;
-		try {
-			retval = thisJoinPoint.proceed();
-		} finally {
-			// measure after
-			final long tout = TIME.getTime();
-			CTRLINST.newMonitoringRecord(new OperationExecutionRecord(signature, sessionId, traceId, tin, tout, hostname, eoi, ess));
-			// cleanup
-			if (entrypoint) {
-				CFREGISTRY.unsetThreadLocalTraceId();
-				CFREGISTRY.unsetThreadLocalEOI();
-				CFREGISTRY.unsetThreadLocalESS();
-			} else {
-				CFREGISTRY.storeThreadLocalESS(ess); // next operation is ess
-			}
-		}
-		return retval;
-	}
+   @Around("monitoredOperation() && notWithinKieker()")
+   public Object operation(final ProceedingJoinPoint thisJoinPoint) throws Throwable { // NOCS (Throwable)
+      if (!CTRLINST.isMonitoringEnabled()) {
+         return thisJoinPoint.proceed();
+      }
+      final String signature = this.signatureToLongString(thisJoinPoint.getSignature());
+      if (!CTRLINST.isProbeActivated(signature)) {
+         return thisJoinPoint.proceed();
+      }
+      // collect data
+      // final boolean entrypoint;
+      // final String hostname = VMNAME;
+      // final String sessionId = SESSIONREGISTRY.recallThreadLocalSessionId();
+      // final int eoi; // this is executionOrderIndex-th execution in this trace
+      final int ess; // this is the height in the dynamic call tree of this execution
+      // long traceId = CFREGISTRY.recallThreadLocalTraceId(); // traceId, -1 if entry point
+      // if (traceId == -1) {
+      // entrypoint = true;
+      // traceId = CFREGISTRY.getAndStoreUniqueThreadLocalTraceId();
+      // CFREGISTRY.storeThreadLocalEOI(0);
+      CFREGISTRY.storeThreadLocalESS(1); // next operation is ess + 1
+      // eoi = 0;
+      // ess = 0;
+      // } else {
+      // entrypoint = false;
+      // eoi = CFREGISTRY.incrementAndRecallThreadLocalEOI(); // ess > 1
+      ess = CFREGISTRY.recallAndIncrementThreadLocalESS(); // ess >= 0
+      // if ((eoi == -1) || (ess == -1)) {
+      // LOGGER.error("eoi and/or ess have invalid values: eoi == {} ess == {}", eoi, ess);
+      // CTRLINST.terminateMonitoring();
+      // }
+      // }
+      // measure before
+      final long tin = TIME.getTime();
+      // execution of the called method
+      final Object retval;
+      try {
+         retval = thisJoinPoint.proceed();
+      } finally {
+         // measure after
+         final long tout = TIME.getTime();
+         CTRLINST.newMonitoringRecord(new OperationExecutionRecord(signature, "1", 1, tin, tout, "HOST", -1, ess));
+         // cleanup
+         // if (entrypoint) {
+         // CFREGISTRY.unsetThreadLocalTraceId();
+         // CFREGISTRY.unsetThreadLocalEOI();
+         // CFREGISTRY.unsetThreadLocalESS();
+         // } else {
+         CFREGISTRY.storeThreadLocalESS(ess); // next operation is ess
+         // }
+      }
+      return retval;
+   }
 }
